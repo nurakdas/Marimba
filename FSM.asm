88633568dbecc57d83ea983b0475fc500a184504
a2k-hanlon <andrewkhanlon@gmail.com>
2020-02-07 19:31:55 -0800

    Initial work on using the internal ADC and other misc. updates

--- FSM.asm
+++ FSM.asm
@@ -13,11 +13,7 @@ CCU_RELOAD  EQU ((65536-((XTAL/(2*CCU_RATE)))))
 ; Serial
 BAUD EQU 115200
 BRVAL EQU ((XTAL/BAUD)-16)
-; SPI
-CE_ADC    EQU  P2.7
-MY_MOSI   EQU  P2.6
-MY_MISO   EQU  P2.5
-MY_SCLK   EQU  P2.4
+
 ; Pin Assignments
 LCD_RS equ P0.5
 LCD_RW equ P0.6
@@ -27,7 +23,7 @@ LCD_D5 equ P1.3
 LCD_D6 equ P1.4
 LCD_D7 equ P1.6
 ; Button ADC channels
-B1_ADC equ P1.7 ;1
+sADC equ P1.7 ;1
 B2_ADC equ P0.0 ;2
 B3_ADC equ P2.1 ;3
 B4_ADC equ P2.0 ;4
@@ -62,9 +58,6 @@ org 0x0023
 
 ; ==============================================================================
 dseg at 0x30
-; PWM output (to oven) variables
-PWM_Duty_Cycle255: ds 1
-PWM_Cycle_Count: ds 1
 ; Timing Variables
 Count10ms: ds 1
 Count1s: ds 2 ; 2 byte value
@@ -97,8 +90,6 @@ BFSM4_timer: ds 1
 x:	ds 4
 y:	ds 4
 bcd:ds 5
-; ADC reading
-MCP3008_reading: ds 2
 
 bseg
 ; Flag set by timer 1 every half second (can be changed if needed)
@@ -159,70 +150,6 @@ Init_SPI:
     mov SPCTL, #11010000B ; Ignore /SS, Enable SPI, DORD=0, Master=1, CPOL=0, CPHA=0, clk/4
     ret
 
-DO_SPI_G:
-	push acc
-	mov R1, #0			  ; Received byte stored in R1
-	mov R2, #8            ; Loop counter (8-bits)
-
-DO_SPI_G_LOOP:
-	mov a, R0             ; Byte to write is in R0
-	rlc a                 ; Carry flag has bit to write
-	mov R0, a
-	mov MY_MOSI, c
-	setb MY_SCLK          ; Transmit
-	mov c, MY_MISO        ; Read received bit
-	mov a, R1             ; Save received bit in R1
-	rlc a
-	mov R1, a
-	clr MY_SCLK
-	djnz R2, DO_SPI_G_LOOP
-	pop acc
-	ret
-
-;Read_MCP3008(channel)
-Read_MCP3008 MAC
-    push ar7
-    mov r7, #%0
-    lcall ?Read_MCP3008
-    pop ar7
-ENDMAC
-
-?Read_MCP3008:
-	mov r0, #1 ; first byte to send
-	clr CE_ADC
-	lcall DO_SPI_G
-	; ignore received data for that frame; it's garbage
-    mov a, r7 ; get channel to read
-    swap a
-    orl a, #0x80
-	mov r0, a ; specify single ended read on channel specified
-	lcall DO_SPI_G
-	mov a, r1 ; r1 holds received byte
-	anl a, #0x03 ; only keep 2 least significant bits
-	mov MCP3008_reading+1, a ; save upper 2 bits
-	mov r0, #0x55 ; don't care what is sent here (so send alternating bits)
-	lcall DO_SPI_G
-	mov a, r1 ; r1 holds received byte
-	mov MCP3008_reading+0, a ; save lower 2 bits
-	setb CE_ADC
-	ret
-
-Calculate_Temp:
-	mov x, MCP3008_reading
-	mov x+1, MCP3008_reading+1
-	mov x+2, #0
-	mov x+3, #0
-	load_y(311669)
-	lcall mul32
-	load_y(1000000)
-	lcall div32
-	lcall hex2bcd
-	; Send_BCD(bcd)
-	; mov a, #'\r'
-	; lcall putchar
-	; mov a, #'\n'
-	; lcall putchar
-	ret
 ; SERIAL =======================================================================
 ; Configure the serial port and baud rate
 InitSerialPort:
@@ -252,41 +179,7 @@ getchar:
 
 ; BUTTONS ======================================================================
 Check_Buttons:
-    Read_MCP3008(B1_ADC)
-    mov a, MCP3008_reading+1 ; get upper byte of ADC reading
-    rr a ; get MSB of reading (the 2^9=512 valued bit)
-    jnz button1_released ; jumpd if the ADC reading is less than 512
-    setb Button1_raw
-    sjmp read_button2
-button1_released:
-    clr Button1_raw
-read_button2:
-    Read_MCP3008(B2_ADC)
-    mov a, MCP3008_reading+1 ; get upper byte of ADC reading
-    rr a ; get MSB of reading (the 2^9=512 valued bit)
-    jnz button2_released ; jumpd if the ADC reading is less than 512
-    setb Button2_raw
-    sjmp read_button3
-button2_released:
-    clr Button2_raw
-read_button3:
-    Read_MCP3008(B3_ADC)
-    mov a, MCP3008_reading+1 ; get upper byte of ADC reading
-    rr a ; get MSB of reading (the 2^9=512 valued bit)
-    jnz button3_released ; jumpd if the ADC reading is less than 512
-    setb Button3_raw
-    sjmp read_button4
-button3_released:
-    clr Button3_raw
-read_button4:
-    Read_MCP3008(B4_ADC)
-    mov a, MCP3008_reading+1 ; get upper byte of ADC reading
-    rr a ; get MSB of reading (the 2^9=512 valued bit)
-    jnz button4_released ; jumpd if the ADC reading is less than 512
-    setb Button4_raw
-    sjmp read_button_done
-button4_released:
-    clr Button4_raw
+    ; TODO: implement reading buttons from a resistor chain on the ADC
 
 read_button_done:
     Button_FSM(BFSM1_state, BFSM1_timer, Button1_raw, B1_flag_bit)
@@ -294,15 +187,21 @@ read_button_done:
     Button_FSM(BFSM3_state, BFSM3_timer, Button3_raw, B3_flag_bit)
     Button_FSM(BFSM4_state, BFSM4_timer, Button4_raw, B4_flag_bit)
     ret
+
 ; MAIN =========================================================================
 main:
 	; Initialization of hardware
     mov SP, #0x7F
-    lcall Timer0_Init
+    lcall Ports_Init ; Default all pins as bidirectional I/O. See Table 42.
     lcall LCD_4BIT
-    lcall InitSerialPort
-    lcall SendString
-    lcall Init_SPI
+    lcall Double_Clk
+	;lcall InitSerialPort ; For sound
+	lcall InitADC0 ; Call after 'Ports_Init'
+	lcall InitDAC1 ; Call after 'Ports_Init'
+	;lcall CCU_Inits ; for sound
+	;lcall Init_SPI ; for sound
+    lcall Timer0_Init
+    lcall Timer1_Init
     setb EA ; Enable Global interrupts
 
     ; Initialize variables
@@ -312,8 +211,7 @@ main:
     mov reflow_temp, #230
     mov reflow_time, #40
     clr a
-    mov PWM_Duty_Cycle255, a
-    mov Count10ms, a
+    mov Count1ms, a
     mov Count1s+0, a
     mov Count1s+1, a
     mov Count_state, a
@@ -326,22 +224,8 @@ main:
     mov BFSM2_timer, a
     mov BFSM3_timer, a
     mov BFSM4_timer,a
-    mov MCP3008_reading+0, a
-    mov MCP3008_reading+1, a
     Load_X(0)
     Load_y(0)
-
-    set_soak_display1:      db 'Set Soak:       ', 0
-    set_reflow_display1:    db 'Set Reflow:     ', 0
-    set_display2:           db 'xxx s at xxx ', 0xDF, 'C ', 0 ; 0xDF is degree symbol code
-
-    display_mode_standby:   db 'Standby   xxx ', 0xDF, 'C', 0
-    display_mode_ramp1:     db 'Ramp 1    xxx ', 0xDF, 'C', 0
-    display_mode_soak:      db 'Soak      xxx ', 0xDF, 'C', 0
-    display_mode_ramp2:     db 'Ramp 2    xxx ', 0xDF, 'C', 0
-    display_mode_reflow:    db 'Reflow    xxx ', 0xDF, 'C', 0
-    display_mode_cooldown:  db 'Cooldown  xxx ', 0xDF, 'C', 0
-    display_mode_error:     db 'ERROR     xxx ', 0xDF, 'C', 0
 
 	; After initialization the program stays in this 'forever' loop
 loop:
@@ -365,8 +249,7 @@ FSM_RESET:
     clr B2_flag_bit
     clr B3_flag_bit
     clr B4_flag_bit
-    Read_MCP3008(0)
-    lcall Calculate_Temp
+
     ; Update temperature reading
     Set_Cursor(1,11)
     mov a, x
